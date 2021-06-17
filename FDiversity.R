c61087dc3829595f58ae61d244e6ff37bedd72d0
kiperry <kiperry1488@gmail.com>
2021-06-17 10:15:45 -0400

    Add resources

---
+++ FDiversity.R
@@ -0,0 +1,83 @@
+#Perry, KI. 2021. Using a functional trait approach to study insect community ecology. Virtual North
+#Central Branch Meeting, Entomological Society of America, June 22.
+
+#https://cran.r-project.org/web/packages/FD/FD.pdf
+
+#Set working directory
+setwd("~/The Ohio State University/Thesis/Data/R/PNR/Tornado")
+
+#Load datasets, one with species abundances and one with species functional traits
+t <-read.csv("Beetle2015_Traits.csv", row.names=1)
+a <-read.csv("Beetle2015_Abund.csv", row.names=1)
+
+#Check the structure of the dataset
+str(t)
+str(a)
+
+#Plot the traits to see if any are redundant
+plot(t)
+
+#head width and elytra length appear correlated with other traits
+#remove redundant traits
+t2 <- t[,-2]
+t2 <- t2[,-5]
+
+install.packages("FD")
+library(FD)
+citation("FD")
+#function dbFD
+
+####################################################################################
+#community-weighted mean trait values
+#include only continuous values, i.e. not categorical traits
+
+#remove dispersal ability (categorical trait) from dataset
+t3 <- t2[,-6]
+
+#calculate CWMs for continuous variables
+cwm <- dbFD(t3[colnames(a), ], a, w.abun = T, stand.x = T)$CWM
+cwm
+
+#Error: Error in dbFD(t3[colnames(a), ], a, w.abun = T, stand.x = T) : 
+#Species x species distance matrix was still is not Euclidean after 'sqrt' correction. Use another correction method
+#There may be additional redundancies within the dataset
+#sqrt is the default correction method;others include "cailliez" and "lingoes" -- see FD package and Laliberte and Legendre 2010
+#stand.x standardizes all traits to mean 0, but traits must be numeric
+cwm <- dbFD(t3[colnames(a), ], a, w.abun = T, stand.x = T, corr = c("cailliez"))$CWM
+cwm
+write.csv(cwm, file = "BeetleCWM.csv")
+
+#CWM values for each site can be used in other analyses to test for treatment effects
+
+
+#####################################################################################
+#Functional diversity indices
+#If dataset has a categorical variable, then calculate a gower dissimilarity matrix
+#Indices will only calculate value if community has at least 3 species
+#Communities that have fewer than 3 species, output will be NA
+
+##Gower dissimilarity matrix
+td<-as.matrix(gowdis(t2))
+
+#Functional richness
+fric <- dbFD(td[colnames(a), ], a, w.abun = T, stand.x = T)$FRic
+fric
+
+#Functional evenness
+feve <- dbFD(td[colnames(a), ], a, w.abun = T, stand.x = T)$FEve
+feve
+
+#Functional divergence
+fdiv <- dbFD(td[colnames(a), ], a, w.abun = T, stand.x = T)$FDiv
+fdiv
+
+#Functional dispersion
+fdis <- dbFD(td[colnames(a), ], a, w.abun = T, stand.x = T)$FDis
+fdis
+
+outputs <- as.data.frame(cbind(fric, feve, fdiv, fdis))
+plot(outputs)
+write.csv(outputs, file = "FDiversity_Indices.csv")
+
+#Functional diversity indices can be used in other analyses to test for treatment effects
+
